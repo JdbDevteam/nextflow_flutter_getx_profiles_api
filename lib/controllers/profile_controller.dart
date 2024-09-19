ed657c2d7ba4b88a1edcf7fe3b378d10534be6a7
Long0017 <js.longthao@gmail.com>
2024-09-19 13:57:24 +0700

    call json data from api success

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -1,12 +1,18 @@
 import 'package:get/get.dart';
+import 'package:nextflow_flutter_getx_profiles_api/models/profile_model/profile_model.dart';
 
 class ProfileController extends GetxController {
   var laoding = false.obs;
+  var profiles = <ProfileModel>[].obs;
 
-  void loadDataFromAPI() {
+  void loadDataFromAPI() async {
     laoding.value = true;
 
-    Future.delayed(Duration(seconds: 3));
+    var cn = Get.find<GetConnect>();
+
+    var response = await cn
+        .get('https://651d740c44e393af2d59d2b4.mockapi.io/api/profiles');
+    print(response.body);
 
     laoding.value = false;
   }
