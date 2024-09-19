0d2d963fa5668e14754f802518b61e612c29c1f3
Long0017 <js.longthao@gmail.com>
2024-09-19 11:42:10 +0700

    add loading

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -1,3 +1,13 @@
 import 'package:get/get.dart';
 
-class ProfileController extends GetxController {}
+class ProfileController extends GetxController {
+  var laoding = false.obs;
+
+  void loadDataFromAPI() {
+    laoding.value = true;
+
+    Future.delayed(Duration(seconds: 3));
+
+    laoding.value = false;
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,16 +3,26 @@ import 'package:get/get.dart';
 import 'package:nextflow_flutter_getx_profiles_api/controllers/profile_controller.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
+  var controller = Get.put(ProfileController());
   @override
   Widget build(BuildContext context) {
+    controller.loadDataFromAPI();
     return Scaffold(
       appBar: AppBar(
         title: Text('Profiles'),
       ),
       // the body contains filter text field and list of profiles
-      body: ListView(),
+      body: Obx(() {
+        if (controller.laoding.value == true) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          return Text("Finish");
+        }
+      }),
     );
   }
 }
